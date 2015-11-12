9172bc9005cf29c1f7c724eafc1c4d85f4bc6548
limjangwon <wkddnjs4058@naver.com>
2015-11-12 14:42:10 +0900

    Transfer

--- src/test/resources/sql/employee.sql
+++ src/test/resources/sql/employee.sql
@@ -1,25 +1,69 @@
-select * from dept;
-select * from country;
-select * from member;
-
-drop table member;
-
 create table member (
-   id            number primary key ,
+   id             int ,
    email          varchar(255),
    name           varchar(255),
    password       varchar(255),
-   register_date  date
-     
+   register_date    date,
+   constraint pk_id primary key (id)   
 );
 
-insert into member(id, email, name, password) values('3','dlwlstn@naver.com','asd','1234');
+create sequence member_id_seq
 
-update member set register_date ='2015/11/11' , password ='1234' where id ='2';
+insert into member(id, email, name, password) 
+values(member_id_seq.nextval,'wkddnjs','장원','1234');
+
+update member set register_date ="2015/11/11" , password ='1234' where id ='2';
 
 alter table member add(aa varchar(255));
 
-delete from member where id='2' ;
+delete from member where id='1' ;
 
 select * from member;
-select * from board;
+
+-- city 
+--selectAll()
+select id, 
+      name,
+      countrycode,
+      district,
+      population
+  from city
+  
+-- selectByCountryCode(String countryCode)
+select id, 
+      name,
+      countrycode,
+      district,
+      population
+  from city
+ where countrycode ='KOR'
+ 
+ --
+ -- city
+ --
+ drop table city;
+ 
+ create table city (
+  id 			numeric(11) ,
+  name 			char(35) ,
+  countrycode	char(3) ,
+  district 		char(20) ,
+  population 	numeric(11) ,
+  constraint pk_id primary key (id)
+)
+
+select * from city;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/resources/sql/world.sql
+++ src/test/resources/sql/world.sql
@@ -1,25 +1,45 @@
-select * from dept;
+select * from city;
 select * from country;
-select * from member;
+select * from city;
 
 drop table member;
 
 create table member (
-   id            number primary key ,
-   email          varchar(255),
+   id              int AUTO_increment,
+   email               varchar(255),
    name           varchar(255),
-   password       varchar(255),
-   register_date  date
-     
+   password        varchar(255),
+   register_date    date,
+   constraint pk_id primary key (id)   
 );
 
-insert into member(id, email, name, password) values('3','dlwlstn@naver.com','asd','1234');
+select max(id)+1 from member;
 
-update member set register_date ='2015/11/11' , password ='1234' where id ='2';
+----------------------------------------
+insert into member(id, email, name, password) values((select max(id)+1 from member),'dlwlstn4308','이진수','1234');
+
+update member set register_date ="2015/11/11" , password ='1234' where id ='2';
 
 alter table member add(aa varchar(255));
 
-delete from member where id='2';
+delete from member where id='2' ;
 
 select * from member;
-select * from board;
+
+-- city 
+--selectAll()
+select id, 
+      name,
+      countrycode,
+      district,
+      population
+  from city
+  
+-- selectByCountryCode(String countryCode)
+select id, 
+      name,
+      countrycode,
+      district,
+      population
+  from city
+ where countrycode ='KOR'
