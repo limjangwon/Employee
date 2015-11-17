81fb4f53714dcbb9e0be16ccbff4608a73a2f1b7
limjangwon <wkddnjs4058@naver.com>
2015-11-17 10:36:21 +0900

    Transfer

--- src/main/java/com/hybrid/mapper/EmpMapper.java
+++ src/main/java/com/hybrid/mapper/EmpMapper.java
@@ -2,9 +2,16 @@ package com.hybrid.mapper;
 
 import java.util.List;
 
-import com.hybrid.model.City;
+import com.hybrid.model.Emp;
 
 
 public interface EmpMapper {
-
+	
+	List<Emp> selectAll();
+	Emp selectByEmpno();
+	
+	List<Emp> selectAllWithDept();
+	Emp selectByEmpnoWithDept();
+	
+	
 }
